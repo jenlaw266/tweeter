080348c5356c45ca75b2e421e5fd7a232e1d491b
Jennifer Law <jenniferlaw@Jennifers-MacBook-Air-2.local>
2021-09-07 15:48:57 -0700

    minor edit: removed comment

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
   $("#tweet-text").on("input", function () {
-    //$(".counter").text(140 - $(this).val().length - 1);
     const $countingDown = $(this).closest("form").find(".counter");
     let charLimit = 140;
     let remainingChar = charLimit - $(this).val().length;
