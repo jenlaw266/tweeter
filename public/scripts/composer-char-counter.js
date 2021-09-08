76acffc6d6fee24618fabf81a72fa54383f692a3
Jennifer Law <jenniferlaw@Jennifers-MacBook-Air-2.local>
2021-09-08 09:20:12 -0700

    fixed nav bar, formatted display tweet

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
   $("#tweet-text").on("input", function () {
-    const $countingDown = $(this).closest("form").find(".counter");
+    const $countingDown = $(this).parent().find(".counter");
     let charLimit = 140;
-    let remainingChar = charLimit - $(this).val().length;
-    if (remainingChar < 0) {
-      $countingDown.css("color", "red");
-    } else {
+    let remainingChar = charLimit - $(this).val().length; //should I re-name $remainingChar
+    if (remainingChar >= 0) {
       $countingDown.css("color", "");
+    } else {
+      $countingDown.css("color", "red");
     }
     $countingDown.text(remainingChar);
   });
